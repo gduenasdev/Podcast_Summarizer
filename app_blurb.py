873813874d442edc4972b088698c067ef7b2a4a6
Gabriel Duenas <duenasdevelops@gmail.com>
2024-01-11 16:26:10 -0800

    Update: add applicaiton description.

---
+++ app_blurb.py
@@ -0,0 +1,7 @@
+blurb = """**Podcast Summarizer** is crafted for podcast enthusiasts \
+    who need to make the most out of their time. Users will quickly \
+    determine the value of a podcast without committing hours to listening. Simply paste \
+    a YouTube URL, and receive a concise summary with a clear overview of the \
+    podcast's content helping you decide if investing 2-3 hours of listening \
+    time is worthwhile. Experience the future of podcast exploration with \
+    **Podcast Summarizer** – your key to unlocking a world of content while saving time."""
---
+++ streamlit_app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain_community.document_loaders import YoutubeLoader
+from app_blurb import blurb
+
+st.title('🎙️⏳ Podcast Summarizer')
+st.markdown(blurb)
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+def generate_response(input_text):
+    llm = OpenAI(model_name="gpt-3.5-turbo-instruct", \
+                 temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+def load_youtube_transcript(url: str):
+    loader = YoutubeLoader.from_youtube_url(
+    url, add_video_info=False
+    )
+    transcript = loader.load()
+    print(type(transcript[0].page_content))
+    print(len(transcript[0].page_content))
+    print(transcript[0].page_content[:5000])
+    # print(len(transcript[0]))
+
+user_input = st.text_input('Enter a youtube podcast url to summarize')
+
+if user_input:  # Check if user_input is not empty
+    if openai_api_key.startswith('sk-'):
+        #generate_response(user_input)  # Call your response generation function
+        load_youtube_transcript(user_input)
+        print('Calling API')
+    else:
+        st.warning('Please enter a valid OpenAI API key!', icon='⚠️')
+else:
+    st.info('Please enter a YouTube URL to get started.')
